182112f584a800659b3fec7e331fb8bdba780d82
Mike Walker <mikewalk@gmail.com>
2021-01-11 22:44:34 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,42 @@
-gamePad.onEvent(GamerBitPin.P14, GamerBitEvent.Down, function () {
-    radio.sendString("L")
-})
-gamePad.onEvent(GamerBitPin.P15, GamerBitEvent.Down, function () {
-    radio.sendString("R")
-})
-gamePad.onEvent(GamerBitPin.P13, GamerBitEvent.Down, function () {
-    radio.sendString("B")
-})
-gamePad.onEvent(GamerBitPin.P8, GamerBitEvent.Down, function () {
-    radio.sendString("F")
-})
-gamePad.onEvent(GamerBitPin.P1, GamerBitEvent.Down, function () {
-    radio.sendString("X")
-})
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "VIBRATESTART") {
-        gamePad.vibratorMotor(gamePad.Vibrator.V1)
+    if (receivedString == "F") {
+        basic.showArrow(ArrowNames.North)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     }
-    if (receivedString == "VIBRATEEND") {
-        gamePad.vibratorMotor(gamePad.Vibrator.V0)
+    if (receivedString == "B") {
+        basic.showArrow(ArrowNames.South)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    }
+    if (receivedString == "L") {
+        basic.showArrow(ArrowNames.East)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    }
+    if (receivedString == "R") {
+        basic.showArrow(ArrowNames.West)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    }
+    if (receivedString == "X") {
+        basic.showIcon(IconNames.No)
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
     }
 })
-gamePad.onEvent(GamerBitPin.P2, GamerBitEvent.Down, function () {
-    radio.sendString("Y")
-})
+basic.showIcon(IconNames.No)
 radio.setGroup(123)
+maqueen.motorStop(maqueen.Motors.All)
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+        radio.sendString("VIBON")
+    } else {
+        radio.sendString("VIBOFF")
+    }
+})
